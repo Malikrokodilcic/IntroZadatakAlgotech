d1bb8a6548316e86b682f2f579d79ed9f3bb9480
Nikola Perlic <Nikola Perlic@DESKTOP-72NFDED>
2024-04-22 18:12:54 +0200

    Add project files.

---
+++ IntroZadatakAlgotech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroZadatakAlgotech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
