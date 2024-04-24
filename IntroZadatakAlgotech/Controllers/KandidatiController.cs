1120213244cc8cb95b0efd7d773e527a42ae5c9e
Никола Перлић <pn200025d@student.etf.bg.ac.rs>
2024-04-24 23:55:14 +0200

    Dodate pocetne verzije controller-a za korisinike i pozicije

---
+++ IntroZadatakAlgotech/Controllers/KandidatiController.cs
@@ -0,0 +1,23 @@
+﻿using IntroZadatakAlgotech.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroZadatakAlgotech.Controllers
+{
+    public class KandidatiController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public KandidatiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var sviKandidati = await _context.Kandidati.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ IntroZadatakAlgotech/Controllers/PozicijeController.cs
@@ -0,0 +1,23 @@
+﻿using IntroZadatakAlgotech.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroZadatakAlgotech.Controllers
+{
+    public class PozicijeController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public PozicijeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var svePozicije = await _context.Pozicije.ToListAsync();
+            return View();
+        }
+    }
+}
